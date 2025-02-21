c229cc8aa6abb50d20500f6c86a038c4945d30e3
VV002 <nikhilzionjuly@gmail.com>
2025-02-21 10:20:46 +0530

    [Nikhhil] Add: Python Script Checks whether the EMployee is Present or Not

---
+++ Employee.py
@@ -0,0 +1,21 @@
+import os
+import random
+
+#Getting the width of the terminal to center the title
+width = os.get_terminal_size().columns
+
+#Printing the title
+print("-------------------------------WELCOME TO EMPLOYEE WAGE COMPUTATION-------------------------------".center(width))
+
+#Function to check the attendance of the employee and partimers
+def Check_Attendance():
+    attendace = random.randint(0,1,2)
+    if attendace == 0:
+        print("Employee is Absent")
+    elif attendace == 1:
+        print("Regular Employee is Present")
+    else:
+        print("Partime Employee is Present")
+
+#Function call to check the attendance
+Check_Attendance()
