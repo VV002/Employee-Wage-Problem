377e82dd119d596cb66710297c106e8266568e3c
VV002 <nikhilzionjuly@gmail.com>
2025-02-24 10:51:36 +0530

    [Nikhil] Add: Python script that generates Employee wage for multiple companies according to user input

--- Employee.py
+++ Employee.py
@@ -4,29 +4,29 @@ import random
 class Employee_wage:
     """
         Description:
-            This class is used to calculate the wage of the employee and partimers.
+            This class is used to calculate the wage of the employee and partimers of multiple companies.
             It has a static method to check the attendance of the employee and partimers.
-            It has a method to calculate the wage of the employee and partimers.
-            It has a method to print the wage of the employee and partimers.
+            It has a method to calculate the wage of the employee and partimers of multiple companies.
+            It has a method to print the wage of the employee and partimers of multiple companies..
         Parameters: 
             None
         Return:
             None
     """
-    max_hours=100
-    max_days=20
-
-    def __init__(self):
+    def __init__(self,price_per_hour,max_hours,max_days):
         """
             Description:
-                This constructor is used to intialize the instance variables.
+                This parameterized constructor is used to intialize the instance variables.
             Parameters:
                 None
             Return:
                 None
         """
+        self.max_hours=max_hours
+        self.max_days=max_days
         self.emp_hours, self.emp_wage,self.emp_days = 0,0,0
         self.partime_days, self.partime_hours, self.partime_wage = 0,0,0
+        self.price_per_hour = price_per_hour
         self.absent_days = 0
         self.total_hours = 0
         self.total_days = 0
@@ -43,7 +43,7 @@ class Employee_wage:
         print(f"Total days worked: {self.total_days} and Total hours worked: {self.total_hours}")
         print(f"Employee worked for {self.emp_days} days and {self.emp_hours} hours and earned ${self.emp_wage}")
         print(f"Partimer worked for {self.partime_days} days and {self.partime_hours} hours and earned ${self.partime_wage}")
-        print(f"Employees was absent for {self.absent_days} days")
+        print(f"Employees was absent for {self.absent_days} days\n")
 
     
     #Static method checks the attendance of the employee and partimers
@@ -63,7 +63,7 @@ class Employee_wage:
         while self.total_hours < self.max_hours and self.total_days < self.max_days:
             if self.Check_Attendance()==1:
                 hours = 8
-                wage = hours * 20
+                wage = hours * self.price_per_hour
                 self.emp_wage+=wage
                 self.emp_hours+=hours
                 self.emp_days+=1
@@ -72,7 +72,7 @@ class Employee_wage:
 
             elif self.Check_Attendance()==2:
                 hours = 4
-                wage = hours * 20
+                wage = hours * self.price_per_hour
                 self.partime_wage+=wage
                 self.partime_hours+=hours
                 self.partime_days+=1
@@ -86,14 +86,30 @@ class Employee_wage:
 
 
 def main():
-
+    """
+        Description:
+            This main method is used to take the input from the user and create the instance of the class.
+        Parameters:
+            None
+        Return:
+            None
+    """
     #Getting the width of the terminal to center the title
     width = os.get_terminal_size().columns
 
     #Printing the titles
     print("-------------------------------WELCOME TO EMPLOYEE WAGE COMPUTATION-------------------------------".center(width))
-    wage = Employee_wage() #instance/object of the class
-    wage.calculate_wage()   
+
+    num = int(input("Enter the number of companies: "))
+    for i in range(num):
+        print(f"----------------------Enter {i+1} Company's Details----------------------\n".center(width))
+        name = input("Enter the name of the company: ")
+        price_per_hour = int(input("Enter the price per hour: "))
+        max_hours = int(input("Enter the maximum working hours: "))
+        max_days = int(input("Enter the maximum working days: "))
+        print(f"----------------------{name} Company's Details----------------------\n".center(width))
+        wage = Employee_wage(price_per_hour,max_hours,max_days) #instance/object of the class
+        wage.calculate_wage()   
 
 
 if __name__ == "__main__":
